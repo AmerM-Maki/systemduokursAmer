f28363e6a9e6b8dfbbc9932dafb61bf695125dc6
AmerM-Maki <amermuhic97@gmail.com>
2024-11-14 21:36:05 +0100

    Y

---
+++ cypress/e2e/1-getting-started/Second-spec.cy.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress" />
+
+describe('Example tests', () => {
+  beforeEach(() => {
+    cy.visit('https://automationexercise.com/')
+  })
+
+  it('Navigate to Contact us', () => {
+    cy.get('a[href="/contact_us"]').should('be.visible').click()
+    cy.url().should('contain', 'contact')
+    cy.get('.col-sm-12').find('h2')
+    cy.get('input').should('be.empty').and('be.visible')
+    cy.get('[data-qa="name"]').type('Name')
+    cy.get('[data-qa="email"]').type('example@gmail.com')
+    cy.get('[data-qa="subject"]').type('Subject')
+    cy.get('[data-qa="message"]').type('ShortMessage')
+    cy.get('[data-qa="submit-button"]').click()
+    cy.get('#form-section').find('.btn-success').click()
+    cy.url().should('not.contain', 'contact')
+  })
+})
